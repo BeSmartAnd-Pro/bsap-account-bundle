2e505914c964b50180686d8b66acc28377de52da
Jakub Roszkiewicz <jroszkiewicz@users.noreply.github.com>
2024-11-03 22:59:19 +0100

    missing bundle class

---
+++ src/BsapAccountBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BeSmartAndPro\BsapAccountBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+final class BsapAccountBundle extends AbstractBundle
+{
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+    }
+}
